c0b304dfc568f556dced5c95b1b5d776c4f5159e
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2020-10-20 12:13:48 +0700

    Adding SplashScreen
    Chả hiểu sao nó bị giật lag lúc bật :D

---
+++ TestForProject1/FoodRecipe/SplashWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace FoodRecipe
+{
+    /// <summary>
+    /// Interaction logic for SplashWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Load(object sender, RoutedEventArgs e)
+        {
+            var value = ConfigurationManager.AppSettings["checkShowDialog"];
+            var showSplash = bool.Parse(value);
+
+            if (showSplash == true)
+            {
+                var screen = new MainWindow();
+                screen.Show();
+                this.Close();
+            }
+        }
+        private void turnOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["checkShowDialog"].Value = "true";
+            config.Save(ConfigurationSaveMode.Minimal);
+        }
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var screen = new MainWindow();
+            screen.Show();
+            this.Close();
+        }
+    }
+}
